ec253f6fffde19e0a2ad5d04167bd1960864121c
Dominik <jurasz.do@gmail.com>
2017-09-13 16:50:27 +0200

    fix commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float launchSpeed;
+
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
